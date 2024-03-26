59de10a10c69f3ad0d1cc077a84e495b40cc25bf
meliance <melakmu66@gmail.com>
2024-03-26 16:02:46 +0300

    betty style corrected

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,9 +7,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively calculate the size of the left and right subtrees */
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	/* Recursively calculate the size of the left and right subtrees */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
